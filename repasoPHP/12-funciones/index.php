cbbc61dd02515f6110026549fa59b4736f3c69d7
Jorge García Gómez <jorge@jorgegarciagomez.es>
2019-08-09 21:36:27 +0200

    funciones

--- repasoPHP/12-funciones/index.php
+++ repasoPHP/12-funciones/index.php
@@ -2,6 +2,8 @@
 
 /* FUNCIONES */
 
+
+/* Ejemplo 1 */
 function muestraNombres() {
 
     echo "Jorge García<br>";
@@ -13,6 +15,53 @@ function muestraNombres() {
 }
 
 muestraNombres();
+
+echo "<hr>";
+
+/* Ejemplo 2 */
+
+function tabla($numero) {
+
+    echo "tabla de multiplicar del número: $numero"."<br>";
+    
+    for ($i=1; $i <= 10 ; $i++) { 
+        echo "$numero x $i = ".($numero*$i)."<br>";
+    }
+}
+
+for ($i=1; $i <= 10 ; $i++) { 
+    tabla($i);
+}
+
+
+echo "<hr>";
+
+/* Ejemplo 3 */
+
+
+function calculadora($numero1, $numero2) {
+    $suma = $numero1+$numero2;
+    $resta = $numero1-$numero2;
+    $multiplicacion = $numero1*$numero2;
+    $division = $numero1/$numero2;
+
+    echo $suma.'<br>';
+    echo $resta.'<br>';
+    echo $multiplicacion.'<br>';
+    echo $division.'<br>';
+
+
+
+}
+
+calculadora(20, 3);
+
+
+
+echo "<hr>";
+
+/* Ejemplo 3 */
+
 
 
 
