d0fc29659edf6524c231936ec3e880864b3a68c9
Jorge García Gómez <jorge@jorgegarciagomez.es>
2019-08-10 17:23:29 +0200

    sesiones

---
+++ repasoPHP/16-sesiones/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+/* Sesion : Almacenar y persistir datos del usuario mientras que navega en el sitio web 
+hasta que cierra sesión o cierra el navegador */
+
+// Iniciar la sesión
+
+session_start();
+
+// Variable de sesion
+
+$_SESSION['variable_persistente'] = "Hola soy una sesion activa";
+
+
+
+
+
+
+
+
+
+
+;?>
