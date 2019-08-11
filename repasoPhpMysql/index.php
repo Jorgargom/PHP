106306aef0f6d1f05e609df29f721ed2388fa202
Jorge García Gómez <jorge@jorgegarciagomez.es>
2019-08-11 20:49:50 +0200

    repaso de conexion a bbdd

--- repasoPhpMysql/index.php
+++ repasoPhpMysql/index.php
@@ -1,19 +1,37 @@
 <?php 
 
 // Conectar a la base de datos
-
 $conexion = mysqli_connect("localhost", "root", "", "phpmysql");
 
 // Comprobar si la conexion es correcta
-
 if (mysqli_connect_errno()) {
     echo "La conexion a la base de datos MYSQL ha fallado: ".mysqli_connect_error();
 } else {
     echo "Conexión OK";
 }
 
+// consulta para configurar la codificación de caracteres
+mysqli_query($conexion, "SET NAMES 'UTF8'");
 
+// Consulta SELECT desde PHP
+$query = mysqli_query($conexion, "SELECT * FROM notas");
 
+while ($notas = mysqli_fetch_assoc($query)) {
+    var_dump($notas);
+}
+
+// Insertar en la BBDD desde PHP
+
+$sql = "INSERT INTO notas VALUES(null, 'Nota desde PHP', 'Esto es una nota desde index.php','green')";
+
+$insert = mysqli_query($conexion, $sql);
+
+if ($insert) {
+    echo "Inserción OK";
+} else {
+    echo "La inserción en la base de datos MYSQL ha fallado: ".mysqli_error($conexion);
+
+}
 
 
 ;?>
