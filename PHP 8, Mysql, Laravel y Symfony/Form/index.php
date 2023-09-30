d3048701bc7f51d26056b886688af578613c66b0
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-09-30 17:48:44 +0200

    form

---
+++ PHP 8, Mysql, Laravel y Symfony/Form/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <div class="contenedor">
+        <form action="validation.php" method="post">
+            <label for="">Correo</label>
+            <br>
+            <input type="email" name="" id="">
+            <br>
+            <label for="">Contraseña</label>
+            <input type="password" name="" id="">
+            <br>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</body>
+</html>
