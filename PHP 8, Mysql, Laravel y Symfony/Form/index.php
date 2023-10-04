945c9e43cf9d147546775f30f5bd8718a91d4fc8
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-10-04 20:55:29 +0200

    Update index.php

--- PHP 8, Mysql, Laravel y Symfony/Form/index.php
+++ PHP 8, Mysql, Laravel y Symfony/Form/index.php
@@ -20,22 +20,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
-</head>
-<body>
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <label for="email">Correo:</label>
-        <br>
-        <input type="email" name="email" required>
-        <br>
-        <label for="password">Contraseña:</label>
-        <br>
-        <input type="password" name="password" required>
-        <br>
-        <input type="submit" value="Enviar">
-    </form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario</title>
+    </head>
+    <body>
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <label for="email">Correo:</label>
+            <br>
+            <input type="email" name="email" required>
+            <br>
+            <label for="password">Contraseña:</label>
+            <br>
+            <input type="password" name="password" required>
+            <br>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
 </html>
