776ae91c1ed35b910d0a90712eb915da3002aa35
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-10-07 21:14:28 +0200

    Update index.php

--- PHP 8, Mysql, Laravel y Symfony/Form/index.php
+++ PHP 8, Mysql, Laravel y Symfony/Form/index.php
@@ -1,21 +1,21 @@
 <?php
-// Verifica si se enviaron los datos del formulario antes de intentar acceder a ellos
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Verifica si las variables POST 'email' y 'password' están definidas antes de asignarlas
-    if (isset($_POST['email']) && isset($_POST['password'])) {
-        $correo = $_POST['email'];
-        $contraseña = $_POST['password'];
+    // Verifica si se enviaron los datos del formulario antes de intentar acceder a ellos
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        // Verifica si las variables POST 'email' y 'password' están definidas antes de asignarlas
+        if (isset($_POST['email']) && isset($_POST['password'])) {
+            $correo = $_POST['email'];
+            $contraseña = $_POST['password'];
 
-        // Escapa los valores para prevenir ataques de inyección de código (SQL u otros)
-        $correo = htmlspecialchars($correo);
-        $contraseña = htmlspecialchars($contraseña);
+            // Escapa los valores para prevenir ataques de inyección de código (SQL u otros)
+            $correo = htmlspecialchars($correo);
+            $contraseña = htmlspecialchars($contraseña);
 
-        // Mostrar un mensaje de bienvenida
-        echo "Bienvenido, tu correo es: $correo y tu contraseña es: $contraseña";
-    } else {
-        echo "No se enviaron todos los datos requeridos.";
+            // Mostrar un mensaje de bienvenida
+            echo "Bienvenido, tu correo es: $correo y tu contraseña es: $contraseña";
+        } else {
+            echo "No se enviaron todos los datos requeridos.";
+        }
     }
-}
 ?>
 
 <!DOCTYPE html>
